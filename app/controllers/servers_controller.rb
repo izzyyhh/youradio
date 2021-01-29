820d7915863615b71c003f5da38a542a57ecbc32
Ismail Halili <ihalili.mmt-b2019@fh-salzburg.ac.at>
2021-01-30 00:19:54 +0100

    add basic auth

--- app/controllers/servers_controller.rb
+++ app/controllers/servers_controller.rb
@@ -1,5 +1,5 @@
 class ServersController < ApplicationController
-    before_action :set_server, only: %i[ show edit update destroy ]
+    before_action :authenticate_user!, :set_server, only: %i[ show edit update destroy ]
 
     def index
         @servers = Server.all
