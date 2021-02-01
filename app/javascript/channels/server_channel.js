31eefe57f6d98468c3db8b3c0df969d6f9bdb108
Ismail Halili <ihalili.mmt-b2019@fh-salzburg.ac.at>
2021-02-01 23:14:44 +0100

    update api index for more data, add working sync of video

--- app/javascript/channels/server_channel.js
+++ app/javascript/channels/server_channel.js
@@ -1,6 +1,7 @@
 import consumer from "./consumer"
 
 // 2. This code loads the IFrame Player API code asynchronously.
+let server_id
 let playlist
 
 var player;
@@ -11,6 +12,13 @@ document.addEventListener('DOMContentLoaded', ()=> {
     player = new YT.Player('player', {
       height: '360',
       width: '640',
+      playerVars: {
+        'autoplay': 1,
+        'controls': 0,
+        'mute': 1,
+        'rel' : 0,
+        'showinfo' : 0
+      },
       videoId: 'M7lc1UVf-VE',
       events: {
         'onReady': onPlayerReady,
@@ -28,7 +36,7 @@ function onPlayerReady(event) {
   //event.target.playVideo();
 
     const element = document.getElementById('server-id');
-    const server_id = element.getAttribute('data-server-id');
+    server_id = element.getAttribute('data-server-id');
   
     consumer.subscriptions.subscriptions.forEach((subscription) => {
       consumer.subscriptions.remove(subscription)
@@ -45,16 +53,79 @@ function onPlayerReady(event) {
         .then((response) => response.json())
         .then((result) => {
           playlist = result
-          let id=playlist[0].uri
-          console.log(id)
-        
-          player.loadVideoById({videoId: id})
+          
+          console.log("playlist")
+          console.log(playlist)
+          let trackToPlay
+          let trackTimePosition
+          
+          if(playlist.length != 0){
+            
+
+            for(let track of playlist){
+              let duration = parseInt(track.duration)
+              let trackStarttime = parseInt(track.starttime)
+              let trackEndtime = trackStarttime + duration
+              let currentTime = Math.floor(Date.now()/1000)
+              console.log(currentTime)
+              console.log(trackEndtime)
+
+              let delta = trackEndtime - currentTime
+              console.log(delta)
+              console.log(duration - delta)
+
+              if(delta > 0 ){
+                //lied ist noch nicht vorbei
+                trackToPlay = track
+                trackTimePosition = duration - delta
+                break
+              }
+            }
+          } else {
+            console.log("kein lied vorhanden")
+          }
+          
+          console.log("dahin")
+          console.log(trackToPlay)
+
+          //let id=playlist[0].uri
+          //console.log(id)
+          let curId
+          if (trackToPlay) {
+            curId=trackToPlay.uri
+          }
+          console.log(trackTimePosition)
+          player.loadVideoById({videoId: curId})
+          
 
           setTimeout(()=> {
-            //loadvideo is async but no promise sadly
+            player.seekTo(trackTimePosition, true)
+            
             console.log(player.getVideoData()['video_id'])         
           }, 300)
+
+
         })
+          
+          
+          
+          // let i = 0;
+          // let trackIndex
+
+          // if (playlist.length != null) {
+          //   for (let track in playlist) {
+          //     console.log(track)
+          //     let trackTime = track.starttime
+          //     console.log(trackTime)
+          //     if (trackTime < currentTime) {
+          //       i++
+          //     } else {
+          //       trackIndex = i-1
+          //       break;
+          //     }
+
+          //   }
+          // }
         
         // Called when the subscription is ready for use on the server
         //bei neuem connect zum server holen wir uns die playlist
@@ -73,10 +144,76 @@ function onPlayerReady(event) {
           messages.innerHTML += data.html
           
         }else {
-          playlist = data.tracks
-          console.log(data.tracks)
-  
+          //playlist = data.tracks
+          //console.log(data.tracks)
           
+          fetch(`/servers/${server_id}/playlist/`)
+          .then((response) => response.json())
+          .then((result) => {
+            playlist = result
+            
+            console.log("playlist")
+            console.log(playlist)
+            let trackToPlay
+            let trackTimePosition
+            
+            if(playlist.length != 0){
+              
+      
+              for(let track of playlist){
+                let duration = parseInt(track.duration)
+                let trackStarttime = parseInt(track.starttime)
+                let trackEndtime = trackStarttime + duration
+                let currentTime = Math.floor(Date.now()/1000)
+                console.log(currentTime)
+                console.log(trackEndtime)
+      
+                let delta = trackEndtime - currentTime
+                console.log(delta)
+                console.log(duration - delta)
+      
+                if(delta > 0 ){
+                  //lied ist noch nicht vorbei
+                  trackToPlay = track
+                  trackTimePosition = duration - delta
+                  break
+                }
+              }
+            } else {
+              console.log("kein lied vorhanden")
+            }
+            
+            console.log("dahin")
+            console.log(trackToPlay)
+      
+            //let id=playlist[0].uri
+            //console.log(id)
+            let curId
+            if (trackToPlay) {
+              curId=trackToPlay.uri
+            }
+            console.log(trackTimePosition)
+            setTimeout(() => {
+              if (player.getPlayerState() === -1) {
+                player.loadVideoById({videoId: curId})
+              }
+            }, 100)
+            
+            
+            console.log("tetetet"+player.getPlayerState())
+            console.log(player.getPlayerState())
+
+            setTimeout(()=> {
+
+              if (player.getPlayerState() === 0) {
+              player.seekTo(trackTimePosition, true)
+              }
+              console.log(player.getVideoData()['video_id'])
+              console.log("adsasd" + player.getPlayerState())         
+            }, 100)
+      
+          })   
+
         }
         // Called when there's incoming data on the websocket for this channel
       }
@@ -90,18 +227,75 @@ function onPlayerReady(event) {
 var done = false;
 function onPlayerStateChange(event) {
   if (event.data == YT.PlayerState.PLAYING && !done) {
-    
+    console.log("started video and runnning")
     //setTimeout(stopVideo, 6000);
     //done = true;
+  } else if (event.data == YT.PlayerState.PAUSED) {
+    even
+  }
+  else if (event.data == YT.PlayerState.ENDED){
+
+    fetch(`/servers/${server_id}/playlist/`)
+    .then((response) => response.json())
+    .then((result) => {
+      playlist = result
+      
+      console.log("playlist")
+      console.log(playlist)
+      let trackToPlay
+      let trackTimePosition
+      
+      if(playlist.length != 0){
+        
+
+        for(let track of playlist){
+          let duration = parseInt(track.duration)
+          let trackStarttime = parseInt(track.starttime)
+          let trackEndtime = trackStarttime + duration
+          let currentTime = Math.floor(Date.now()/1000)
+          console.log(currentTime)
+          console.log(trackEndtime)
+
+          let delta = trackEndtime - currentTime
+          console.log(delta)
+          console.log(duration - delta)
+
+          if(delta > 0 ){
+            //lied ist noch nicht vorbei
+            trackToPlay = track
+            trackTimePosition = duration - delta
+            break
+          }
+        }
+      } else {
+        console.log("kein lied vorhanden")
+      }
+      
+      console.log("dahin")
+      console.log(trackToPlay)
+
+      //let id=playlist[0].uri
+      //console.log(id)
+      let curId
+      if (trackToPlay) {
+        curId=trackToPlay.uri
+      }
+      console.log(trackTimePosition)
+      player.loadVideoById({videoId: curId})
+      
+
+      setTimeout(()=> {
+        player.seekTo(trackTimePosition, true)
+        console.log(player.getVideoData()['video_id'])         
+      }, 300)
+
+    })
   }
 }
 
 function stopVideo() {
-  player.stopVideo();
+  //player.stopVideo();
 }
-
-
-
 
 
 
