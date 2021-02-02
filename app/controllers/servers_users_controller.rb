e78c32ff3030600d8f9ff212ccd2c2ef8f892268
Ismail Halili <ihalili.mmt-b2019@fh-salzburg.ac.at>
2021-02-02 14:30:43 +0100

    prepare deploy

--- app/controllers/servers_users_controller.rb
+++ app/controllers/servers_users_controller.rb
@@ -5,23 +5,23 @@ class ServersUsersController < ApplicationController
     end
 
     def create
-        new_member_assoc = ServersUser.new
-        new_member = User.where(email: params[:user_email]).first
-        
-        if new_member.nil?
-            redirect_to request.referrer, notice: "member couldn't be found, please check the email adress"
-            return
-        end
-
+        users_selected_emails = params[:users]
         affected_server =  Server.find(params[:servers_user][:server_id])
 
-        new_member_assoc.user_id = new_member.id
-        new_member_assoc.server_id = affected_server.id
+        users_selected_emails.each do |email|
+            new_member_assoc = ServersUser.new
+            new_member = User.where(email: email).first
 
-        if new_member_assoc.save
-            redirect_to affected_server, notice: "member added successfully"
-        else
-            redirect_to affected_server, notice: "failed adding member"
+            new_member_assoc.user_id = new_member.id
+            new_member_assoc.server_id = affected_server.id
+
+            if !new_member_assoc.save
+                redirect_to affected_server, notice: "failed adding member"
+                return
+            end
+            
         end
+
+        redirect_to affected_server, notice: "added succesfully"
     end
 end
