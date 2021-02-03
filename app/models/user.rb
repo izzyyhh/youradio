315e41b0ba1b1c495f43a84193f4880c03bec297
Ismail Halili <ihalili.mmt-b2019@fh-salzburg.ac.at>
2021-02-03 22:13:33 +0100

    attach pic and set validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-    has_and_belongs_to_many :servers
+  has_and_belongs_to_many :servers
+  has_one_attached :profilepic
+
+  validates :profilepic, blob: { content_type: ['image/png', 'image/jpg', 'image/jpeg'], size_range: 1..5.megabytes }
+
 end
