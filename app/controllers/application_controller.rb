7f48c67bdaf660e8251feb9dc6bd03fa1bd10657
Ismail Halili <ihalili.mmt-b2019@fh-salzburg.ac.at>
2021-02-03 22:13:54 +0100

    permit pic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < ActionController::Base
     protected
 
          def configure_permitted_parameters
-              devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation)}
+              devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :profilepic, :email, :password, :password_confirmation)}
 
-              devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :password_confirmation)}
+              devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :profilepic, :email, :password, :current_password, :password_confirmation)}
          end
 end
