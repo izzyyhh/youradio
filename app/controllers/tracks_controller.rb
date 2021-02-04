311589039a5ee49352eef222bf1c1e9821d719d7
web-valentin <valentin.kiefl@kiefl.de>
2021-02-04 23:18:13 +0100

    validation for uri in tracks controller

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -11,71 +11,103 @@ class TracksController < ApplicationController
   def create
     apiKey = ENV.fetch("API_KEY")
     #ENV['DATABASE_URL']
-    p "-----------------"
     uri = params[:track][:uri]
-    youtube_id = get_youtube_id(uri)
-    url = "https://www.googleapis.com/youtube/v3/videos?id=#{youtube_id}&part=contentDetails&key=#{apiKey}"
-    details_url = "https://www.googleapis.com/youtube/v3/videos?part=snippet&id=#{youtube_id}&key=#{apiKey}"
-    response = RestClient.get(url)
-
-    p "------------------"
-    p JSON.parse(response, object_class: OpenStruct)
-    struct = JSON.parse(response, object_class: OpenStruct)
-    details = JSON.parse(RestClient.get(details_url), object_class: OpenStruct)
-
-    title = details.items[0].snippet.title
-    description = details.items[0].snippet.description
-    thumbnail = details.items[0].snippet.thumbnails.medium.url
-    channeltitle = details.items[0].snippet.channelTitle
-    #mehr details title, description, thumbnail,  channeltitle
-    duration = struct.items[0].contentDetails.duration
-    duration_in_s = convert_time(duration)
-
+    uri_useable= validate_uri(uri)
     server_id = params[:track][:server_id]
-    @playlist = Server.find(server_id).playlist
+    
+    if uri_useable
 
-    p "----------------------"
-    if (@playlist.tracks.count == 0)
-      @track = Server.find(server_id).playlist.tracks.create(uri: youtube_id, duration: duration_in_s,
-        starttime: Time.now, title: title,
-        channeltitle: channeltitle, description: description,
-        thumbnail: thumbnail)
+      youtube_id = get_youtube_id(uri)
+      url = "https://www.googleapis.com/youtube/v3/videos?id=#{youtube_id}&part=contentDetails&key=#{apiKey}"
+      details_url = "https://www.googleapis.com/youtube/v3/videos?part=snippet&id=#{youtube_id}&key=#{apiKey}"
+      response = RestClient.get(url)
+      struct = JSON.parse(response, object_class: OpenStruct)
+      details = JSON.parse(RestClient.get(details_url), object_class: OpenStruct)
+
+      #try catch block
+
+      begin 
+
+        # get video details from api
+        title = details.items[0].snippet.title
+        description = details.items[0].snippet.description
+        thumbnail = details.items[0].snippet.thumbnails.medium.url
+        channeltitle = details.items[0].snippet.channelTitle
+        #mehr details title, description, thumbnail,  channeltitle
+        duration = struct.items[0].contentDetails.duration
+        duration_in_s = convert_time(duration)
+
+
+        server_id = params[:track][:server_id]
+        @playlist = Server.find(server_id).playlist
+
+        # time calculation
+
+        # if playlist empty
+        if (@playlist.tracks.count == 0)
+          @track = Server.find(server_id).playlist.tracks.create(uri: youtube_id, duration: duration_in_s,
+            starttime: Time.now, title: title,
+            channeltitle: channeltitle, description: description,
+            thumbnail: thumbnail)
+          p "das erste im empty"
+
+        # if playlist has entries
+        else 
+          p time = @playlist.tracks.last.starttime + @playlist.tracks.last.duration.seconds
+          p Time.now
+          p (time - Time.now).seconds
+          time = @playlist.tracks.last.starttime + @playlist.tracks.last.duration.seconds
+
+          # if playlist has already stopped but has entries which already have been played
+          if !@playlist.tracks.first || (time - Time.now).seconds < 0
+            @track = Server.find(server_id).playlist.tracks.create(uri: youtube_id, duration: duration_in_s,
+              starttime: Time.now, title: title,
+              channeltitle: channeltitle, description: description,
+              thumbnail: thumbnail)
+          
+          # if playlist has songs and play a song
+          else
+            
+            last_track = @playlist.tracks.last
+            old_starttime = last_track.starttime 
+            old_duration = last_track.duration
+            this_starttime= old_starttime + old_duration.seconds
+            @track = Server.find(server_id).playlist.tracks.create(uri: youtube_id, duration: duration_in_s,
+                                                            starttime: this_starttime, title: title,
+                                                            channeltitle: channeltitle, description: description,
+                                                            thumbnail: thumbnail)
+            p 'first track existent'
+          end
+
+        end
+        SendTrackJob.perform_later(@playlist)
+      rescue
+        p "Die eigegebene Url liefert keine Daten"
         
-      p "das erste im empty"
-    else 
-      p time = @playlist.tracks.last.starttime + @playlist.tracks.last.duration.seconds
-      p Time.now
-      p (time - Time.now).seconds
-
-      time = @playlist.tracks.last.starttime + @playlist.tracks.last.duration.seconds
-
-      if !@playlist.tracks.first || (time - Time.now).seconds < 0
-        @track = Server.find(server_id).playlist.tracks.create(uri: youtube_id, duration: duration_in_s,
-          starttime: Time.now, title: title,
-          channeltitle: channeltitle, description: description,
-          thumbnail: thumbnail)
-
-        p 'first trackkkkkkkkkkkk creating for the time'
-      else
-        p "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-        last_track = @playlist.tracks.last
-        old_starttime = last_track.starttime 
-        old_duration = last_track.duration
-        this_starttime= old_starttime + old_duration.seconds
-        @track = Server.find(server_id).playlist.tracks.create(uri: youtube_id, duration: duration_in_s,
-                                                        starttime: this_starttime, title: title,
-                                                        channeltitle: channeltitle, description: description,
-                                                        thumbnail: thumbnail)
-        p 'first track existent'
       end
-
+    else
+      p "Url ist empty oder nil-> so geht das nicht"
+    
     end
-
-
-    SendTrackJob.perform_later(@playlist)
   end
 
   private
+
+  def validate_uri(uri)
+    #check if input is empty
+    if uri != "" and uri != nil
+      id = get_youtube_id(uri)
+
+      #check if youtube id is empty
+      if id != "" and id != nil
+        return true
+      else
+        return false
+      end
+    else 
+      return false
+    end 
+  end
 
   def tracks_params
     params.require(:track).permit(:uri)
