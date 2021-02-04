e3c1c455e3627736f687fcdfdaa4a9a1a3da5034
Ismail Halili <ihalili.mmt-b2019@fh-salzburg.ac.at>
2021-02-04 20:23:39 +0100

    beautify modal

--- app/javascript/packs/youtubeplayer.js
+++ app/javascript/packs/youtubeplayer.js
@@ -11,7 +11,6 @@ document.addEventListener('DOMContentLoaded', () => {
     addTrackSection = document.querySelector('.add-track-section')
     overlay = document.querySelector('.add-track__overlay')
     addTrack = document.getElementById('addtrack')
-    disappear()
 
     addTrack.addEventListener('click', () => {
         disappear()
@@ -34,13 +33,13 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 function disappear() {
-    addTrackSection.style.visibility="hidden"
+    addTrackSection.style.transform = "translate(-43%, -50%) scale(0)";
     overlay.style.visibility="hidden"
     isOpen = false
 }
 
 function appear() {
-    addTrackSection.style.visibility="initial"
+    addTrackSection.style.transform = "translate(-43%, -50%) scale(1)";
     overlay.style.visibility="initial"
     isOpen = true
 }
