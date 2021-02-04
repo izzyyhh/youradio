39c19c40f5946597a30b38b1ec41a3bce880bf0c
web-valentin <valentin.kiefl@kiefl.de>
2021-02-04 17:17:56 +0100

    finish styling of modal

--- app/javascript/packs/youtubeplayer.js
+++ app/javascript/packs/youtubeplayer.js
@@ -1,11 +1,26 @@
 console.log("hi")
 let addTrackSection
 let isOpen = false
+let overlay
+let addTrack
 
 document.addEventListener('DOMContentLoaded', () => {
+
+    
+
     addTrackSection = document.querySelector('.add-track-section')
+    overlay = document.querySelector('.add-track__overlay')
+    addTrack = document.getElementById('addtrack')
     disappear()
 
+    addTrack.addEventListener('click', () => {
+        disappear()
+    })
+
+
+    overlay.addEventListener('click', () => {
+        disappear()
+    })
     let addTrackButton = document.querySelector('.musicbar__link')
     addTrackButton.addEventListener('click', () => {
 
@@ -20,10 +35,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function disappear() {
     addTrackSection.style.visibility="hidden"
+    overlay.style.visibility="hidden"
     isOpen = false
 }
 
 function appear() {
     addTrackSection.style.visibility="initial"
+    overlay.style.visibility="initial"
     isOpen = true
 }
