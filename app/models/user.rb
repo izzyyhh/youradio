c1baee4387768c73701362c936791dad1ce586f7
Ismail Halili <ihalili.mmt-b2019@fh-salzburg.ac.at>
2021-03-04 11:23:18 +0100

    refactor at add-new-user-to-server-view, move logic from view to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,10 @@ class User < ApplicationRecord
   has_one_attached :profilepic
 
   validates :profilepic, blob: { content_type: ['image/png', 'image/jpg', 'image/jpeg'], size_range: 1..5.megabytes }
+
+  def self.users_not_added_to_server(server_id)
+    users = User.all
+    users_already_added = User.joins(:servers_users).where(servers_users: { server_id: server_id })
+    users_not_added = users.reject { |u| users_already_added.include?(u) }
+  end
 end
