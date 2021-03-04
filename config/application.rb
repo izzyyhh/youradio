d19b633c468140f0e56f586cd6e9a6db13ad5c4f
Ismail Halili <ihalili.mmt-b2019@fh-salzburg.ac.at>
2021-03-04 12:15:42 +0100

    rm another style/documentation issue

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-#:nodoc:
 require_relative 'boot'
 
 require 'rails/all'
@@ -10,6 +9,7 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module TestRadio
+  #:nodoc:
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
