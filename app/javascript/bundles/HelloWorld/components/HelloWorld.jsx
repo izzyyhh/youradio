baab4f5ba383fdb5e985bf99d9a25917cd35ae67
web-valentin <valentin.kiefl@kiefl.de>
2021-05-02 18:19:51 +0200

    add redux and test store

--- app/javascript/bundles/HelloWorld/components/HelloWorld.jsx
+++ app/javascript/bundles/HelloWorld/components/HelloWorld.jsx
@@ -1,25 +1,44 @@
 import PropTypes from 'prop-types';
-import React, { useState } from 'react';
+import React from 'react';
+import { combineReducers } from 'redux'
 
-const HelloWorld = (props) => {
-  const [name, setName] = useState(props.name);
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
 
-  return (
-    <div>
-      <h3>Hello, {name}!</h3>
-      <hr />
-      <form>
-        <label htmlFor="name">
-          Say hello to:
-          <input id="name" type="text" value={name} onChange={(e) => setName(e.target.value)} />
-        </label>
-      </form>
-    </div>
-  );
-};
+const rootReducer = combineReducers({
+  user: () => []
+})
+
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )
+)
+
+window.store = store
+
+
+const HelloWorld = ({ name, updateName }) => (
+  <div>
+    <h3>
+      Hello,
+      {name}!
+    </h3>
+    <hr />
+    <form>
+      <label htmlFor="name">
+        Say hello to:
+        <input id="name" type="text" value={name} onChange={(e) => updateName(e.target.value)} />
+      </label>
+    </form>
+  </div>
+);
 
 HelloWorld.propTypes = {
-  name: PropTypes.string.isRequired, // this is passed from the Rails view
+  name: PropTypes.string.isRequired,
+  updateName: PropTypes.func.isRequired,
 };
 
 export default HelloWorld;
