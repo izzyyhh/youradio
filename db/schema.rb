081b3a1d228f8f6b3030fd91a32c5fd41a7a60ff
Ismail Halili <ihalili.mmt-b2019@fh-salzburg.ac.at>
2021-05-04 21:07:47 +0200

    add working version of explore page WITHOUT server owner

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_02_200349) do
+ActiveRecord::Schema.define(version: 2021_05_04_095256) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2021_02_02_200349) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "is_public", default: true
   end
 
   create_table "servers_users", force: :cascade do |t|
