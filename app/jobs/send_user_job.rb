bbf4e54c271526179317875da8302476d1bb02d6
web-valentin <valentin.kiefl@kiefl.de>
2021-05-05 10:50:53 +0200

    add userlist api in controller

---
+++ app/jobs/send_user_job.rb
@@ -0,0 +1,7 @@
+class SendUserJob < ApplicationJob
+    queue_as :default
+  
+    def perform(server_id)
+      ActionCable.server.broadcast("reaction_channel_#{server_id}", { reactionType: "active" })
+    end
+end
