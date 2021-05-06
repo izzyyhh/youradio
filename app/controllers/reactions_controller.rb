29784521a6a8da134be93a28283d735fe3fe0fbe
web-valentin <valentin.kiefl@kiefl.de>
2021-05-06 10:23:09 +0200

    add user reactions over reacting user and animate - issue 20

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -8,6 +8,7 @@ class ReactionsController < ApplicationController
   def create
     reaction_type = params[:reaction][:reactionType]
     server_id = params[:reaction][:server_id]
-    ActionCable.server.broadcast("reaction_channel_#{server_id}", { reactionType: reaction_type })
+    user_reacting = params[:reaction][:currentUser]
+    ActionCable.server.broadcast("reaction_channel_#{server_id}", { reactionType: reaction_type, userReacting: user_reacting })
   end
 end
