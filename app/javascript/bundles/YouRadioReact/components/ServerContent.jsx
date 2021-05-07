94aa2a392605bee697bc0932c4a2f6de302d0ae7
Ismail Halili <ihalili.mmt-b2019@fh-salzburg.ac.at>
2021-05-07 10:17:52 +0200

    update ActiveUser test

--- app/javascript/bundles/YouRadioReact/components/ServerContent.jsx
+++ app/javascript/bundles/YouRadioReact/components/ServerContent.jsx
@@ -1,19 +1,17 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import UserReactions from './UserReactions'
-import UserPresence from './UserPresence'
 
 const ServerContent = ({serverName}) => {
     return (
     <div className='content'>
         {/* <h2>Welcome to {serverName}</h2> */}
         <UserReactions/>
-        {/* <UserPresence/> */}
     </div>
 )};
 
 ServerContent.propTypes = {
-   serverName: PropTypes.string.isRequired,
+   serverName: PropTypes.string,
 };
 
 export default ServerContent;
--- test/javascript/YouRadioReact/ActiveUser.test.js
+++ test/javascript/YouRadioReact/ActiveUser.test.js
@@ -15,12 +15,12 @@ describe("ActiveUser", () => {
     expect(container.querySelector("h1")).toBeTruthy();
   });
 
-  it("component renders as a list element", () => {
+  it("component renders as a list element, when user are given", () => {
     const { container } = render(<ActiveUser user={user}></ActiveUser>);
     expect(container.querySelector("li")).toBeTruthy();
   });
 
-  it("list id should be user id", () => {
+  it("list item id should be user id", () => {
     const { container } = render(<ActiveUser user={user}></ActiveUser>);
     const id = container.querySelector("li").getAttribute("id")
     expect(id).toBe(user.id.toString())
