808cb7a4eb18ed98eb0034e42f409f5ff0d28ff3
Ismail Halili <ihalili.mmt-b2019@fh-salzburg.ac.at>
2021-05-21 12:27:47 +0200

    refactor userreactions component

--- app/javascript/bundles/YouRadioReact/components/UserReactions.jsx
+++ app/javascript/bundles/YouRadioReact/components/UserReactions.jsx
@@ -6,78 +6,44 @@ import Reaction from '../../../packs/reaction_class'
 import Button from './Button'
 import Counter from './Counter'
 import ActiveUser from './ActiveUser'
-import { unescapeComponent } from 'uri-js';
+import {LIKE_REACTION, DISLIKE_REACTION, RESET_REACTION, HEART_REACTION, FIRE_REACTION, ACTIVE_REACTION } from '../../../packs/constants'
 
+const getUser = async (setUserList) => {
+	const data = await fetch(`/servers/${server_id}/userlist/`)
+	const userlist = await data.json()
+	setUserList(userlist)
+}
 
-// mvp version of reactions, counters are not saved in backend
-// thus users, will have different counters, when joined on different times
-let likes = 0;
-let dislikes = 0;
-let heart = 0;
-let fire = 0;
-let list = [];
-const LIKE_REACTION = 'LIKE'
-const DISLIKE_REACTION = 'DISLIKE'
-const HEART_REACTION = 'HEART'
-const FIRE_REACTION = 'FIRE'
-const RESET_REACTION = 'RESET'
-
-const reactionInstance = new Reaction(likes, dislikes, heart, fire)
-
-const UserReactions = () => {
-	const [reaction, setReaction] = useState(reactionInstance)
-	const [userList, setUserList] = useState(list)
+const useReactions = (setUserList, userList) => {
+	const [reactions, setReactions] = useState(new Reaction())
 
 	useEffect(() => {
 		ReactionChannel.received = (data) => {
-			switch(data.reactionType) {
-				case LIKE_REACTION:
-					likes +=1
-					reactionInstance.likeCounter = likes
-					setReaction(Object.assign({}, reactionInstance))
-					displayReaction(data)
-					break;
-				case DISLIKE_REACTION:
-					dislikes+=1
-					reactionInstance.dislikeCounter = dislikes
-					setReaction(Object.assign({}, reactionInstance))
-					displayReaction(data)
-					break;
-				case HEART_REACTION:
-					heart +=1
-					reactionInstance.heartCounter = heart
-					setReaction(Object.assign({}, reactionInstance))
-					displayReaction(data)
-					break;
-				case FIRE_REACTION:
-					fire +=1
-					reactionInstance.fireCounter = fire
-					setReaction(Object.assign({}, reactionInstance))
-					displayReaction(data)
-					break;
-				case RESET_REACTION:
-					// a broadcast is sent, when video finishes, then
-					// reaction counters must be reset
-					likes = 0, dislikes = 0, heart = 0, fire = 0
-					reactionInstance.reset()
-					setReaction(Object.assign({}, reactionInstance))
-					break;
-				case "active":
-					getUser()
-					break;
+			if(data.reactionType === ACTIVE_REACTION) {
+				getUser(setUserList)
+			} else {
+				reactions.handleReaction(data)
+				console.log("reaction")
+				console.log(reactions)
+				setReactions({...reactions})
+				displayReaction(data)
 			}
 		}
-	}, [likes, dislikes, heart, fire, userList])
+	}, [userList])
+
+	return reactions
+}
+
+const UserReactions = () => {
+	const [userList, setUserList] = useState([])
+
+	const reaction = useReactions(setUserList, userList)
 
 	useEffect(() => {
-		getUser()
+		getUser(setUserList)
 	}, [])
 
-	const getUser = async () => {
-		const data = await fetch(`/servers/${server_id}/userlist/`)
-		const userlist = await data.json()
-		setUserList(userlist)
-	}
+
 
 	const handleReaction = async (reactionType) => {
 		await fetch('/reactions', {
@@ -102,18 +68,10 @@ const UserReactions = () => {
 				<div className='player-space'></div>
 				<div className="action-wrapper">
 					<div className='content_reactionbuttonline'>
-						<div className='counterleft-wrapper'>
-							<Counter id='heartCounter' className='counter-box'>{reaction.heartCounter} <i className="fas fa-heart"></i></Counter>
-							<Counter id='fireCounter' className='counter-box'>{reaction.fireCounter} <i className="fas fa-fire-alt"></i></Counter>
-						</div>
-							<Button onClick={() => handleReaction(HEART_REACTION)}><i className="fas fa-heart"></i></Button>
-							<Button onClick={() => handleReaction(FIRE_REACTION)}><i className ="fas fa-fire-alt"></i></Button>
-							<Button onClick={() => handleReaction(LIKE_REACTION)}><i className="fas fa-thumbs-up"></i></Button>
-							<Button onClick={() => handleReaction(DISLIKE_REACTION)}><i className="fas fa-thumbs-down"></i></Button>
-						<div className="counterright-wrapper">
-							<Counter id='likeCounter' className='counter-box'>{reaction.likeCounter} <i className="fas fa-thumbs-up"></i></Counter>
-							<Counter id='dislikeCounter' className='counter-box'>{reaction.dislikeCounter} <i className="fas fa-thumbs-down"></i></Counter>
-						</div>
+						<Button onClick={() => handleReaction(HEART_REACTION)}><Counter id='heartCounter' className='counter-box'>{reaction.heartCounter}<i className="fas fa-heart"></i></Counter></Button>
+						<Button onClick={() => handleReaction(FIRE_REACTION)}><Counter id='fireCounter' className='counter-box'>{reaction.fireCounter} <i className="fas fa-fire-alt"></i></Counter></Button>
+						<Button onClick={() => handleReaction(LIKE_REACTION)}><Counter id='likeCounter' className='counter-box'>{reaction.likeCounter} <i className="fas fa-thumbs-up"></i></Counter></Button>
+						<Button onClick={() => handleReaction(DISLIKE_REACTION)}><Counter id='dislikeCounter' className='counter-box'>{reaction.dislikeCounter} <i className="fas fa-thumbs-down"></i></Counter></Button>
 					</div>
 				</div>
 			</div>
@@ -124,7 +82,7 @@ const UserReactions = () => {
 			</ul>
 		</>
 	)
-	}
+}
 
 UserReactions.propTypes = {
 };
--- app/javascript/packs/reaction_class.js
+++ app/javascript/packs/reaction_class.js
@@ -1,14 +1,41 @@
+import {LIKE_REACTION, DISLIKE_REACTION, FIRE_REACTION, HEART_REACTION, RESET_REACTION, ACTIVE_REACTION} from './constants'
+
+
 export default class Reaction {
-  constructor(like, dislike, heart, fire) {
-    this.likeCounter = like;
-    this.dislikeCounter = dislike;
-    this.heartCounter = heart;
-    this.fireCounter = fire;
+  constructor() {
+    this.likeCounter = 0;
+    this.dislikeCounter = 0;
+    this.heartCounter = 0;
+    this.fireCounter = 0;
   }
   reset() {
     this.likeCounter = 0;
     this.dislikeCounter = 0;
     this.heartCounter = 0;
     this.fireCounter = 0;
+  }
+
+  increment(type) {
+    switch(type) {
+      case LIKE_REACTION:
+        this.likeCounter++
+        break;
+      case DISLIKE_REACTION:
+        this.dislikeCounter++
+        break;
+      case HEART_REACTION:
+        this.heartCounter++
+        break;
+      case FIRE_REACTION:
+        this.fireCounter++
+        break;
+      case RESET_REACTION:
+        this.reset()
+      break;
+    }
+  }
+
+  handleReaction(data) {
+    this.increment(data.reactionType)
   }
 }
